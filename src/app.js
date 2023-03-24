e72165e1512dc3b5c5a4d5b42ecf9738f3669e9e
Maryam <1maryamrezaei@gmail.com>
2023-03-24 21:28:48 +0430

    API Integration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 function displayTemperature(response) {
-  console.log(response.data.temperature.current);
+  //console.log(response.data.temperature.feels_like);
+
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
 }
 
 let apiKey = "f1b32fef6035o8eb44384ft470f76aee";
