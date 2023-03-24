aa53b321a2f0f61e5834eb153ecae9be8f4becaf
Maryam <1maryamrezaei@gmail.com>
2023-03-24 23:03:27 +0430

    add date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   //console.log(response.data.temperature.feels_like);
 
@@ -18,9 +41,12 @@ function displayTemperature(response) {
 
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "f1b32fef6035o8eb44384ft470f76aee";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Tehran&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=tehran&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
